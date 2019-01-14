85e97dd79c9526b504e65749b89cdc9a189592e1
americanman <snowboardracer7@gmail.com>
2019-01-13 19:23:36 -0500

    Convert data types

--- python.py
+++ python.py
@@ -4,3 +4,12 @@ def add_numbers(a, b):
     print(a + b)
 
 add_numbers(5, 5)
+
+# To convert foloat to int or vise versa
+# call a funciton with prepended name of datatype 
+
+print("Convert 3.14 to integer")
+print(int(3.14))
+
+print("Convert integer 10 to float")
+print(float(10))
